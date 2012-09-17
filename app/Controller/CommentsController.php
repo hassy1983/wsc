c30d85a3e31fc0c881b886de0c13577ef92c060c
hassy <hassy1983@gmail.com>
2012-09-17 19:06:45 +0900

    Create Comments MVC.
    CommentのMVCを作った。

---
+++ app/Controller/CommentsController.php
@@ -0,0 +1,100 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Comments Controller
+ *
+ * @property Comment $Comment
+ */
+class CommentsController extends AppController {
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Comment->recursive = 0;
+		$this->set('comments', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		$this->Comment->id = $id;
+		if (!$this->Comment->exists()) {
+			throw new NotFoundException(__('Invalid comment'));
+		}
+		$this->set('comment', $this->Comment->read(null, $id));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Comment->create();
+			if ($this->Comment->save($this->request->data)) {
+				$this->flash(__('Comment saved.'), array('action' => 'index'));
+			} else {
+			}
+		}
+		$users = $this->Comment->User->find('list');
+		$services = $this->Comment->Service->find('list');
+		$this->set(compact('users', 'services'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		$this->Comment->id = $id;
+		if (!$this->Comment->exists()) {
+			throw new NotFoundException(__('Invalid comment'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Comment->save($this->request->data)) {
+				$this->flash(__('The comment has been saved.'), array('action' => 'index'));
+			} else {
+			}
+		} else {
+			$this->request->data = $this->Comment->read(null, $id);
+		}
+		$users = $this->Comment->User->find('list');
+		$services = $this->Comment->Service->find('list');
+		$this->set(compact('users', 'services'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws MethodNotAllowedException
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		$this->Comment->id = $id;
+		if (!$this->Comment->exists()) {
+			throw new NotFoundException(__('Invalid comment'));
+		}
+		if ($this->Comment->delete()) {
+			$this->flash(__('Comment deleted'), array('action' => 'index'));
+		}
+		$this->flash(__('Comment was not deleted'), array('action' => 'index'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
---
+++ app/Model/Comment.php
@@ -0,0 +1,87 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Comment Model
+ *
+ * @property User $User
+ * @property Service $Service
+ */
+class Comment extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'service_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'star' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Service' => array(
+			'className' => 'Service',
+			'foreignKey' => 'service_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
